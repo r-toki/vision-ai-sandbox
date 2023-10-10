d3c31e4998c520b6c2797a6460d7707bd8097a80
r-toki <natto.and.eggs@gmail.com>
2023-10-10 16:17:10 +0900

    fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,9 @@
 import { onRequest } from "firebase-functions/v2/https";
+import { setGlobalOptions } from "firebase-functions/v2/options";
+
+setGlobalOptions({
+  region: "asia-northeast1",
+});
 
 export const helloWorld = onRequest((_req, res) => {
   res.send("Hello from Firebase!");
